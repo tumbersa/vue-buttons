513902fd0fcb07fa438538e61ead90ace55816f5
Gleb K <gleb.kapustin.98@mail.ru>
2024-04-12 15:38:01 +0300

    added musicianGrid

--- vue/src/components/pages/blogPohomovaKapustin/components/Musician/MusicianCell.vue
+++ vue/src/components/pages/blogPohomovaKapustin/components/Musician/MusicianCell.vue
@@ -1,14 +1,93 @@
 <template>
-    <div>
-      <img :src="musician.imageUrl" :alt="musician.name" class="album__image" />
+  <div class="musician-cell">
+    <img :src="musician.imageUrl" :alt="musician.name" class="album__image" />
+    <div class="info">
+      <div class="name">
+        <img :src="imgNameSrc" :alt="imgName" class="img__name" />
+        {{ musician.name }}
+      </div>
+      <div class="age">
+        <img :src="imgAgeSrc" :alt="imgAge" class="img__age" />
+        {{ musician.age }}
+      </div>
+      <div class="genre">
+        <img :src="imgGenreSrc" :alt="imgGenre" class="img__genre" />
+        {{ musician.genre }}
+      </div>
+      <div class="listenersPerMonth">
+        <img :src="imgListenersSrc" :alt="imgListeners" class="img__listeners" />
+        {{ musician.listenersPerMonth }}
+      </div>
     </div>
+  </div>
 </template>
-    
+
 <script>
+import { imgNameSrc, imgAgeSrc, imgGenreSrc, imgListenersSrc } from '@/components/pages/blogPohomovaKapustin/constantsPK.js';
 
+export default {
+  props: {
+    musician: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    imgNameSrc() {
+      return imgNameSrc;
+    }, 
+    imgAgeSrc() {
+      return imgAgeSrc;
+    },
+    imgGenreSrc() {
+      return imgGenreSrc;
+    },
+    imgListenersSrc() {
+      return imgListenersSrc;
+    }
+  }
+};
 </script>
-    
-<style scoped lang="less">
 
+<style scoped lang="less">
+.musician-cell {
+  display: flex;
+  align-items: center;
+}
+
+.album__image {
+  width: 200px;
+  height: 200px;
+  margin-right: 10px;
+  border-radius: 15px;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  font-family: "Verdana";
+  color: lightgray;
+  font-size: 23px;
+}
+
+.img__name {
+  width: 20px;
+  height: 20px;
+}
+
+.img__age {
+  width: 20px;
+  height: 20px;
+}
+
+.img__genre {
+  width: 20px;
+  height: 20px;
+}
+
+.img__listeners {
+  width: 20px;
+  height: 20px;
+}
 </style>
-  
--- vue/src/components/pages/blogPohomovaKapustin/components/Musician/MusicianGrid.vue
+++ vue/src/components/pages/blogPohomovaKapustin/components/Musician/MusicianGrid.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        <MusicianCell v-for="(musician, index) in musicians" :key="index" :musician="musician" />
+        <div class="title">
+            Исполнители
+        </div>
+        <div class="grid-container">
+            <MusicianCell v-for="(musician, index) in musicians" :key="index" :musician="musician" />
+        </div>
     </div>
 </template>
-    
+
 <script>
 import MusicianCell from './MusicianCell.vue'
 export default {
@@ -13,11 +18,35 @@ export default {
     data() {
         return {
             musicians: [
-                { name: "D.K.", age: "27", genre: "rave", listenersPerMonth: "414 314", imageUrl: "https://previews.dropbox.com/p/thumb/ACPEkfN_dRgkDAJoY9jQfeV-NtdcxbJ00oRjvGR4RiDELvx0eAP2_z9Nlpl_rsXUIF6MF-gOkv4Jhy7vCCLBQMlowD3BkeVuikg_6Nr_xUPCVOCAAxNgWsLwllel0RT369LTdmOoh22QR7y632r0LfeL8_cHMDVJwyw2c2Fz4Q7L2iTN_RAMjBoFWemaAFn0VJSuQ5Ko_GW_fZ1bl-CDVU9Xaw0Uh60zHdE1XRID612awglQrbjROlOhKqGfrx3F6piBF4rcAjAfKY8TKRMIXjm8GQhciNLRO7ensGcXIwL2yv6kIBxSU6mc8d_Z99-y3Bb2rk3zlfARcLb9SAKeQoj2/p.jpeg" },
+                { name: "D.K.", age: "27", genre: "rave", listenersPerMonth: "414 314", imageUrl: "https://previews.dropbox.com/p/thumb/ACPrF3SJGolcXh27UgDJQv5Gfqv8V8k4uMAyA2D8UpdoUpKebhwOuhs_LB0A3rf1wQe249RhGmLPza-WPLWaBq7dtQiCXIHfd6RM-uoZxd5bCyFLIhmzkD4KAhBSQOmyqh2_ezspM2zudv-4w9FI0Imb4E-ymIFBny2039gJ1C96d9yWXY9hzJv61iSpacNn2ebWohvmmm7948sEaQ8IRM_-tG1TaD7AlSubQ_qfvyZ63Q-sLOs8ir4jjuo7k2HpukUCKo3JBb2yFgNiWC6aQTDL_Vrl0HrGOl41yxgOmfEM0VgmlnZpOPV3lNC_dl2wH5R7z8n8QvrVEtodKJyYN0H-/p.jpeg" },
+                { name: "Boulevard Depo", age: "32", genre: "rap", listenersPerMonth: "1 604 753", imageUrl: "https://previews.dropbox.com/p/thumb/ACPoErkHaxeLGAMnagpfzDz_ay38kSsdV5MrXXaHWYpRZ4cgVaG7K7KiC7kX_xT_KLv-i1V-aBJDNAWJTrOzttUkfWSXLdgqk8Td8-Os8a3PQHAdugkOjg7V5qj5mXwuz_SLzarabGWpdalxLXOOYoUuHN8tlqoHOcyX-MFmqkEaOfn-3pFsw9RvkEOn-0fchTty_s_pYsX09OuprB_BiVZlryMDlbrrfZwsw2obKyI2LhaARHLeA5BpKFlUr1vEuT7a06ndkmx7XoUldoWT0Uo_6nOBcMOFW5nCYB7CyTxBFCD9zLunsKbqs5tlD54X2cWhBZbJd2EoUfp_zY1MmxHj/p.jpeg" },
+                { name: "Джизус", age: "26", genre: "rock", listenersPerMonth: "708 069", imageUrl: "https://previews.dropbox.com/p/thumb/ACPz7zlMJd9u3xBXj8GmmGmORNOJ1lavt1MdomhvS3yXmPGgXAVSf9DyCUbzgCmnM37Vd-aZ9JFOlM3eWlizcGB9A_NXDTWe8QROKWbotN3y4Dol04nR9Wy913zCkHTFVPmHVomwmjQR7b60QIo4hG2HSfZzv93M-cvvWBixILtkJEAHt3kxxaQZhD2t1qEGcYh-MO4nS3EgNL55-4XyQbuoFFrf4L7TZ2diJ0OPZworaoCP_cDwa0ASc8EuKaqZUEgLlvoUDpo7OyMxVsX8a1aRKfpwAYzfnHQifFCOT6wCKOplR0gcPEYCHyRxuzu3aaQFoTLOH8ha2w74CMCkliqr/p.jpeg" },
+                { name: "Кишлак", age: "25", genre: "rap", listenersPerMonth: "977 873", imageUrl: "https://previews.dropbox.com/p/thumb/ACMExU9uJjyB5crgen4yt8j1D_54gpACVQQLsGw6FS6SVq0wwnbZMpU5JGu4htBjrKYl-mL7EJpe37uQ1tuQbqSE4WsMpO_P3oj6JLl8AEqVeSvNPC2UhjY8VUGUZmfj6qwmDxXV_tZD3S1HgAoyVnEYche6MDncZLYL2CtbLrmzBd6Q1g2-k7FSup9IuoF8L6rcdNEe4HoNZVwVKol2HLCsZ-RXyf_mm2MU3xZ8n3mC7p74e7Pm-MvGUAl1BIrnhOSl2SmrGuXNK-Yfcv24RqBnUwmdIceWR6QPcZhN5UNFrXLOFLYSRVhjJm2dXga2buor0uMNDaKQhSHLRJ-uu_k2Oi4Q0LBiYiOOupw99O2xzg/p.jpeg" },
+                { name: "D.K.", age: "27", genre: "rave", listenersPerMonth: "414 314", imageUrl: "https://previews.dropbox.com/p/thumb/ACPrF3SJGolcXh27UgDJQv5Gfqv8V8k4uMAyA2D8UpdoUpKebhwOuhs_LB0A3rf1wQe249RhGmLPza-WPLWaBq7dtQiCXIHfd6RM-uoZxd5bCyFLIhmzkD4KAhBSQOmyqh2_ezspM2zudv-4w9FI0Imb4E-ymIFBny2039gJ1C96d9yWXY9hzJv61iSpacNn2ebWohvmmm7948sEaQ8IRM_-tG1TaD7AlSubQ_qfvyZ63Q-sLOs8ir4jjuo7k2HpukUCKo3JBb2yFgNiWC6aQTDL_Vrl0HrGOl41yxgOmfEM0VgmlnZpOPV3lNC_dl2wH5R7z8n8QvrVEtodKJyYN0H-/p.jpeg" },
+                { name: "D.K.", age: "27", genre: "rave", listenersPerMonth: "414 314", imageUrl: "https://previews.dropbox.com/p/thumb/ACPrF3SJGolcXh27UgDJQv5Gfqv8V8k4uMAyA2D8UpdoUpKebhwOuhs_LB0A3rf1wQe249RhGmLPza-WPLWaBq7dtQiCXIHfd6RM-uoZxd5bCyFLIhmzkD4KAhBSQOmyqh2_ezspM2zudv-4w9FI0Imb4E-ymIFBny2039gJ1C96d9yWXY9hzJv61iSpacNn2ebWohvmmm7948sEaQ8IRM_-tG1TaD7AlSubQ_qfvyZ63Q-sLOs8ir4jjuo7k2HpukUCKo3JBb2yFgNiWC6aQTDL_Vrl0HrGOl41yxgOmfEM0VgmlnZpOPV3lNC_dl2wH5R7z8n8QvrVEtodKJyYN0H-/p.jpeg" },
+                { name: "D.K.", age: "27", genre: "rave", listenersPerMonth: "414 314", imageUrl: "https://previews.dropbox.com/p/thumb/ACPrF3SJGolcXh27UgDJQv5Gfqv8V8k4uMAyA2D8UpdoUpKebhwOuhs_LB0A3rf1wQe249RhGmLPza-WPLWaBq7dtQiCXIHfd6RM-uoZxd5bCyFLIhmzkD4KAhBSQOmyqh2_ezspM2zudv-4w9FI0Imb4E-ymIFBny2039gJ1C96d9yWXY9hzJv61iSpacNn2ebWohvmmm7948sEaQ8IRM_-tG1TaD7AlSubQ_qfvyZ63Q-sLOs8ir4jjuo7k2HpukUCKo3JBb2yFgNiWC6aQTDL_Vrl0HrGOl41yxgOmfEM0VgmlnZpOPV3lNC_dl2wH5R7z8n8QvrVEtodKJyYN0H-/p.jpeg" },
             ]
         }
     }
 }
 </script>
-    
-<style scoped lang="less"></style>
+
+<style scoped lang="less">
+.title {
+    color: white;
+    font-family: "Lucida Handwriting", cursive;
+    font-size: 30px;
+    text-align: left;
+    letter-spacing: 5px;
+    margin-left: 10%;
+    margin-bottom: 20px;
+    margin-top: 40px;
+}
+
+.grid-container {
+    display: grid;
+    margin-left: 10%;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+}
+</style>
