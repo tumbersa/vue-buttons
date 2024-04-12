795d5702680da46586db549b47b859ccfa1ee0f5
Gleb K <gleb.kapustin.98@mail.ru>
2024-04-13 00:36:12 +0300

    added footer

--- vue/src/components/pages/blogPohomovaKapustin/BlogPohomovaKapustinHome.vue
+++ vue/src/components/pages/blogPohomovaKapustin/BlogPohomovaKapustinHome.vue
@@ -5,6 +5,7 @@
         <AlbumTitle />
         <AlbumList />
         <MusicianGrid />
+        <FooterPK />
     </div>
 </template>
 
@@ -14,6 +15,7 @@ import HatPohomovaKapustin from "./components/Top/HatPohomovaKapustin.vue"
 import AlbumTitle from './components/Album/AlbumTitle.vue'
 import AlbumList from './components/Album/AlbumList.vue'
 import MusicianGrid from './components/Musician/MusicianGrid.vue'
+import FooterPK from './components/Bottom/FooterPK.vue'
 
 export default {
     name: "BlogPohomovaKapustinHome",
@@ -22,7 +24,8 @@ export default {
         HatPohomovaKapustin,
         AlbumTitle,
         AlbumList,
-        MusicianGrid
+        MusicianGrid,
+        FooterPK
     }
 };
 </script>
---
+++ vue/src/components/pages/blogPohomovaKapustin/components/Bottom/FooterPK.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="footer">
+        <img :src="imgTgSrc" :alt="footer_alt" class="footer__image" />
+        <div class="footer__text">@p_chel_enok</div>
+    </div>
+</template>
+
+<script>
+import { imgTgSrc } from '@/components/pages/blogPohomovaKapustin/constantsPK.js';
+export default {
+    computed: {
+        imgTgSrc() {
+            return imgTgSrc
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.footer {
+    height: 70px;
+    background-color: black;
+    color: white;
+    font-family: "Verdana";
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    align-items: center;
+}
+
+.footer__image {
+    width: 50px;
+    height: 50px;
+}
+
+.footer__text {
+    background-color: black;
+    flex: 1;
+    text-align: center; 
+}
+</style>
