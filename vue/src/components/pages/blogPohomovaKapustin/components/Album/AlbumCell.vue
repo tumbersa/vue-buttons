b62e60efafe591972e8e703a758667bbdc5c6d38
Галина Иванова <gleb.kapustin.98@mail.ru>
2024-04-12 12:19:06 +0300

    added scrolling

--- vue/src/components/pages/blogPohomovaKapustin/components/Album/AlbumCell.vue
+++ vue/src/components/pages/blogPohomovaKapustin/components/Album/AlbumCell.vue
@@ -1,49 +1,47 @@
 <template>
-    <div class="album-row">
-      <div v-for="(album, index) in albums" :key="index" class="album__container">
-        <div class="artist">{{ album.artist }}</div>
-        <div class="image-container">
-          <img :src="album.imageUrl" :alt="album.title" class="album__image" />
-          <div class="title">{{ album.title }}</div>
-        </div>
-      </div>
+  <div class="album__container">
+    <div class="artist">{{ album.artist }}</div>
+    <div class="image-container">
+      <img :src="album.imageUrl" :alt="album.title" class="album__image" />
+      <div class="title">{{ album.title }}</div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      albums: {
-        type: Array,
-        required: true
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    album: {
+      type: Object,
+      required: true
     }
-  };
-  </script>
-  
-  <style lang="less">
-  .album-row {
-    white-space: nowrap; 
   }
-  
-  .album__container {
-    display: inline-block;
-  }
-  
-  .album__image {
-    width: 300px;
-    height: auto;
-  }
-  
-  .artist {
-    font-family: "Verdana";
-    color: white;
-    font-size: 23px;
-  }
-  
-  .title {
-    font-family: "Verdana";
-    color: lightgray;
-    font-size: 23px;
-  }
-  </style>
+};
+</script>
+
+<style lang="less">
+.album__container {
+  display: inline-block;
+}
+
+.album__image {
+  width: 300px;
+  height: auto;
+}
+
+.artist {
+  font-family: "Verdana";
+  color: white;
+  font-size: 23px;
+  overflow-wrap: break-word; 
+  max-width: 300px; 
+}
+
+.title {
+  font-family: "Verdana";
+  color: lightgray;
+  font-size: 23px;
+  overflow-wrap: break-word; 
+  max-width: 300px; 
+}
+</style>
